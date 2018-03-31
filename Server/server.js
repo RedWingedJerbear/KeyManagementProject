cea67c96dfae92dc39a3d6fabb7782d0b6cb3d9b
white <white@nwmissouri.edu>
2018-03-31 15:25:17 -0500

    created the server file and a mongoose model for key requests.

---
+++ Server/server.js
@@ -0,0 +1,37 @@
+// Set up
+var express  = require('express');
+var app      = express();                               // create our app w/ express
+var mongoose = require('mongoose');                     // mongoose for mongodb
+var morgan = require('morgan');             // log requests to the console (express4)
+var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+var cors = require('cors');
+
+//Configuration
+mongoose.connect('mongodb://localhost/keys');
+
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
+app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+//Models
+var KeyRequest = mongoose.model('KeyRequest',{
+    request_date : date,
+    request_department : string,
+    request_email : string,
+    request_name : string,
+    request_919 : string,
+    request_number : string,
+    request_keys : string,
+    request_authorized : boolean
+})
